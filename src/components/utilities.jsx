2949743284c45c3e2ca11c83e444335784ca215f
rc-balaji <balajic@drngpit.ac.in>
2023-10-15 17:49:53 +0530

    sss

--- src/components/utilities.jsx
+++ src/components/utilities.jsx
@@ -3,17 +3,29 @@ export const drawRect = (detections, ctx) => {
     const [x, y, width, height] = prediction["bbox"];
     const text = prediction["class"];
 
-
-      const color = "green";
-      ctx.strokeSylt = color;
-      ctx.font = "20px Arial";
-      ctx.fillStyle = color;
-
-      ctx.beginPath();
-      ctx.fillText(text+"(Good)", x, y);
-      ctx.rect(x, y, width, height);
-     
-      ctx.stroke();
+    // Define the color and text styles based on the label
+    let color;
+    let label;
+    if (["apple", "orange", "banana"].includes(text)) {
+      color = "green";
+      label = text.substring(0, 1).toUpperCase() + text.substring(1) + " (Good)";
+    } else if (["sandwich", "hot dog", "donut", "cake", "pizza"].includes(text)) {
+      color = "red";
+      label = text.substring(0, 1).toUpperCase() + text.substring(1) + " (Bad)";
+    } else {
+      // Handle other labels as needed
+      color = "blue";
+      label = text;
     }
-  );
+
+    // Apply styles and draw the rectangle and label
+    ctx.strokeStyle = color;
+    ctx.font = "20px Arial";
+    ctx.fillStyle = color;
+
+    ctx.beginPath();
+    ctx.fillText(label, x, y);
+    ctx.rect(x, y, width, height);
+    ctx.stroke();
+  });
 };
