5212c53afe83c001c118c98cefa72677ca8c413b
rc-balaji <balajic@drngpit.ac.in>
2023-10-15 17:24:27 +0530

    sss

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -57,7 +57,7 @@ export function Scanner() {
       <button
   onClick={switchCamera}
   className="btn btn-primary"
-//   style={{ position: "absolute", top: "10px", right: "10px" }}
+  style={{ position: "absolute", top: "100px", right: "10px" }}
 >
   Switch Camera
 </button>
