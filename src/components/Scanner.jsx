84c9791d1709aeba60c7036f2c00722da56cb833
rc-balaji <balajic@drngpit.ac.in>
2023-10-15 17:15:12 +0530

    updated

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,88 +1,63 @@
-// Import dependencies
-import React, { useRef, useState, useEffect } from "react";
-import * as tf from "@tensorflow/tfjs";
-// 1. TODO - Import required model here
-// e.g. import * as tfmodel from "@tensorflow-models/tfmodel";
-import * as cocossd from "@tensorflow-models/coco-ssd"
+import React, { useRef, useEffect } from "react";
+import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
-// import "./App.css";
-
 import { drawRect } from "./utilities";
 import { Nav } from "./Nav/Nav";
-// 2. TODO - Import drawing utility here
-// e.g. import { drawRect } from "./utilities";
 
 export function Scanner() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-  // Main function
   const runCoco = async () => {
-    // 3. TODO - Load network 
-    // e.g. const net = await cocossd.load();
     const net = await cocossd.load();
-    //  Loop and detect hands
     setInterval(() => {
       detect(net);
     }, 10);
   };
 
   const detect = async (net) => {
-    // Check data is available
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
-      // Get Video Properties
       const video = webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
 
-      // Set video width
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
 
-      // Set canvas height and width
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
-      // 4. TODO - Make Detections
-      // e.g. const obj = await net.detect(video);
       const obj = await net.detect(video);
-      console.log(obj)
-      // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
-
-      // 5. TODO - Update drawing utility
-      // drawSomething(obj, ctx)  
-      drawRect(obj,ctx)
+      drawRect(obj, ctx);
     }
   };
 
-  useEffect(()=>{runCoco()},[]);
+  useEffect(() => {
+    runCoco();
+  }, []);
 
   return (
     <div className="App">
-      <Nav/>
+      <Nav />
       <header className="App-header">
-        <Webcam
-          ref={webcamRef}
-          muted={true} 
-         
-          
-        />
+        <div style={{ position: "relative" }}>
+          <Webcam ref={webcamRef} muted={true} />
 
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-          }}
-        />
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+            }}
+          />
+        </div>
       </header>
     </div>
   );
 }
-
