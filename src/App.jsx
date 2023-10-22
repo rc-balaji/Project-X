473b6d5dcf1206a47653450ac8d0be1b90421958
rc-balaji <balajic@drngpit.ac.in>
2023-10-22 15:22:34 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-
- {Home} from './components/Home';
-import Login from './components/Login';
-import { Detector } from './components/Detector';
-import { Scanner } from './components/Scanner';
-import { Profile } from './components/Profile';
-import Register from './components/Register';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import { Nav } from './components/Nav/Nav';
+import { Home } from "./components/Home";
+import Login from "./components/Login";
+import { Detector } from "./components/Detector";
+import { Scanner } from "./components/Scanner";
+import { Profile } from "./components/Profile";
+import Register from "./components/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Nav } from "./components/Nav/Nav";
 
 function App() {
   return (
-    <div >
-      <BrowserRouter >
-      {/* <Nav/> */}
+    <div>
+      <BrowserRouter>
+        {/* <Nav/> */}
         <Routes>
-          <Route path="/" element ={<Register/>} />
-          <Route path="/register" element ={<Register/>} />
-          <Route path="/login" element ={<Login/>} />
-          <Route path="/home" element ={<Home/>} />
-          <Route path="/detector" element ={<Detector/>} />
-          <Route path="/scanner" element ={<Scanner/>} />
-          <Route path="/profile" element ={<Profile/>} />
+          <Route path="/" element={<Register />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/detector" element={<Detector />} />
+          <Route path="/scanner" element={<Scanner />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -7,13 +6,17 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     axios
-      .post("https://health-server-bms1.onrender.com/login", { email, password })
+      .post("https://health-server-bms1.onrender.com/login", {
+        email,
+        password,
+      })
       .then((result) => {
         console.log(result);
         if (result.data === "Success") {
@@ -28,52 +31,60 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <div
-        className="d-flex justify-content-center align-items-center text-center vh-100"
-        style={{
-          backgroundImage:
-            "linear-gradient(#00d5ff,#0095ff,rgba(93,0,255,.555))",
-        }}
-      >
-        <div className="bg-white p-3 rounded" style={{ width: "40%" }}>
-          <h2 className="mb-3 text-primary">Welcome to ARHealthEats</h2>{" "}
-          <h4 className="mb-4">Login</h4>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3 text-start">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                <strong>Email Id</strong>
-              </label>
-              <input
-                type="email"
-                placeholder="Enter Email"
-                className="form-control"
-                id="exampleInputEmail1"
-                onChange={(event) => setEmail(event.target.value)}
-                required
-              />
+    <div className="container py-5 h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div
+            className="card shadow-2-strong"
+            style={{ borderRadius: "1rem", backgroundColor: "#f0ca32" }}
+          >
+            <div className="card-body p-5 text-center">
+              <h3 className="mb-5">Sign in</h3>
+
+              <form onSubmit={handleSubmit}>
+                <div className="form-outline mb-4">
+                  <input
+                    type="email"
+                    id="typeEmail"
+                    className="form-control form-control-lg"
+                    placeholder="Email address"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <label className="form-label" htmlFor="typeEmail">
+                    Email
+                  </label>
+                </div>
+
+                <div className="form-outline mb-4">
+                  <input
+                    type="password"
+                    id="typePassword"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <label className="form-label" htmlFor="typePassword">
+                    Password
+                  </label>
+                </div>
+
+                <button
+                  className="btn btn-primary btn-lg btn-block"
+                  type="submit"
+                >
+                  Login
+                </button>
+              </form>
+
+              <hr className="my-4" />
+
+              <div className="text-center">
+                <p>
+                  Not a member? <Link to="/register">Register</Link>
+                </p>
+              </div>
             </div>
-            <div className="mb-3 text-start">
-              <label htmlFor="exampleInputPassword1" className="form-label">
-                <strong>Password</strong>
-              </label>
-              <input
-                type="password"
-                placeholder="Enter Password"
-                className="form-control"
-                id="exampleInputPassword1"
-                onChange={(event) => setPassword(event.target.value)}
-                required
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Login
-            </button>
-          </form>
-          <p className="container my-2">Don&apos;t have an account?</p>
-          <Link to="/register" className="btn btn-secondary">
-            Register
-          </Link>
+          </div>
         </div>
       </div>
     </div>
